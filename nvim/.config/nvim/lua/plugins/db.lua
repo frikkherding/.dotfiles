31b6e73c7a672ff93e5db0045751136a7f62fbc5
Frikk Herding <frikksol@gmail.com>
2026-03-03 11:01:43 +0100

    Add shortcut for DB mgmt in vim

--- nvim/.config/nvim/lua/plugins/db.lua
+++ nvim/.config/nvim/lua/plugins/db.lua
@@ -2,6 +2,14 @@ return { -- Database management plugins
   {
     'tpope/vim-dadbod',
     'kristijanhusak/vim-dadbod-ui',
-    { 'kristijanhusak/vim-dadbod-completion', lazy = true },
+    {
+      'kristijanhusak/vim-dadbod-completion',
+      lazy = true,
+      opts = {
+        keys = {
+          vim.keymap.set('n', '<leader>D', '<CMD>DBUIToggle<CR>', { desc = 'Toggle DB Mgmt' }),
+        },
+      },
+    },
   },
 }
