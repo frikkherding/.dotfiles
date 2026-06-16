0518bb2decb2c98e5c1242b4d9a91d6f05d2f877
Frikk Herding <frikksol@gmail.com>
2026-06-16 10:07:59 +0200

    nvim: switch to master branch to fix treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 return { -- Highlight, edit, and navigate code
   'nvim-treesitter/nvim-treesitter',
+  branch = 'master', -- Legacy branch; `main` dropped the nvim-treesitter.configs API
   build = ':TSUpdate',
   main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
